55390ec7fd9a923d3c71b910e1d61ee7699b9a64
Yasen-Getzov-09 <yasen.getzov@icloud.com>
2025-04-25 17:21:48 +0300

    Aliens moving to right.

--- alien_invasion.py
+++ alien_invasion.py
@@ -34,6 +34,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens
             self._update_screen()
             self.clock.tick(60)
 
@@ -75,11 +76,15 @@ class AlienInvasion:
         """Update position of bullets and get rid of old bullets."""
         # Update bullet positions.
         self.bullets.update()
+    
 
         # Get rid of bullets that have disappeared.
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+    def _update_aliens(self):
+        """Update the positions of all aliens in the fleet"""
+        self.aliens.update()
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
