7d06f3c0d8259f541ed4b68cefd4b9c8447127f3
Yasen-Getzov-09 <yasen.getzov@icloud.com>
2025-04-25 17:39:35 +0300

    Update aliens movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -83,9 +83,10 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
     def _update_aliens(self):
-        """Update the positions of all aliens in the fleet"""
+        """Check if the fleet is at an edge, then update positons."""
+        self._check_fleet_edges()
         self.aliens.update()
-
+        
     def _create_fleet(self):
         """Create the fleet of aliens."""
         # Create an alien and keep adding aliens until there's no room left.
@@ -110,6 +111,18 @@ class AlienInvasion:
         new_alien.rect.x = x_position
         new_alien.rect.y = y_position
         self.aliens.add(new_alien)
+        
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached the edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            self.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
