4a521f211cfa6b9a7b2d76e8d30cdf4e7f6de831
Yasen-Getzov-09 <yasen.getzov@icloud.com>
2025-04-28 16:56:47 +0300

    Scoring system develop

--- settings.py
+++ settings.py
@@ -33,6 +33,8 @@ class Settings:
         
         # fleet_direction of 1 represents right; -1 represents left.]
         self.fleet_direction = 1
+        # Scoring settings.
+        self.alien_points = 50
     
     def increase_speed(self):
         """Increase speed settings."""
