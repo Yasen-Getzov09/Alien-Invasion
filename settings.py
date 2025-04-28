a2c599fc7cd77271d27f9917c0ded19fe695450b
Yasen-Getzov-09 <yasen.getzov@icloud.com>
2025-04-28 16:42:42 +0300

    Increasing ship, bullets value to 3 and 7

--- settings.py
+++ settings.py
@@ -8,14 +8,14 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
         # Ship settings
-        self.ship_speed = 1.5
+        self.ship_speed = 3
         self.ship_limit = 3
         # Bullet settings
         self.bullet_speed = 5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60 ,60)
-        self.bullets_allowed = 3
+        self.bullets_allowed = 7
         
         # Alien settings
         self.alien_speed = 1.0
@@ -27,7 +27,7 @@ class Settings:
 
     def initialize_dynamic_settings(self):
         """Initiliaze settings that change throughout the game."""
-        self.ship_speed = 1.5
+        self.ship_speed = 3
         self.bullet_speed = 2.5
         self.alien_speed = 1.0
         
