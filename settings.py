5dd8ee990c15250f5bfd8f7b043795b4fe7c19ec
Yasen-Getzov-09 <yasen.getzov@icloud.com>
2025-04-29 19:01:34 +0300

    In settings.py the bullets_color changed from black to white.

--- settings.py
+++ settings.py
@@ -17,7 +17,7 @@ class Settings:
         self.bullet_speed = 5
         self.bullet_width = 3
         self.bullet_height = 15
-        self.bullet_color = (60, 60 ,60)
+        self.bullet_color = (255, 255, 255)
         self.bullets_allowed = 7
         
         # Alien settings
